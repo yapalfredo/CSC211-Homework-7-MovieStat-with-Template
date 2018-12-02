e7f9ef491a1ed6241b36c6bd88dbde4fca7d3b8e
yapalfredo <36459847+yapalfredo@users.noreply.github.com>
2018-12-02 12:42:44 -0500

    12/2/2018 template version of MovieStat.. working

---
+++ CSC211-Homework-7-MovieStat-with-Template/main.cpp
@@ -0,0 +1,22 @@
+#include "MovieStat.h"
+
+int main()
+{
+
+	MovieStat<int> mv;
+	ofstream outFile;
+
+	try
+	{
+		outFile.open("Homework6p2-2-2.txt");
+		mv.loadScreen(outFile);
+		outFile.close();
+	}
+	catch (exception& e)
+	{
+		//DO NOTHING
+	}
+
+	system("Pause");
+	return 0;
+}
